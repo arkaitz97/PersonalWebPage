a324ea852d7d51b3b32f2b79a0866515e968faca
Arkaitz Molinuevo <arkaitz@arkaitzmolinuevo.com>
2022-02-25 13:55:28 +0100

    Changed default menu position to bottom

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,14 @@
+<script>
+	import Menu from '../components/Menu.svelte';
+</script>
 <div>
+	<Menu/>
 </div>
 <style>
 	div{
 		display: grid;
 		justify-items: center;
-		align-items: center;
+		align-items: end;
 		height: 100vh;
 	}
 </style>

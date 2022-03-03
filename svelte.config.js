9f3ab0cd98ea9fd9a4d7276c09dac163f83ccc5f
Arkaitz Molinuevo <arkaitz@arkaitzmolinuevo.com>
2022-03-03 13:19:41 +0100

    Fixed build

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 //import adapter from '@sveltejs/adapter-auto';
-import cloudflare from "@sveltejs/adapter-cloudflare";
+import cloudflare from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {

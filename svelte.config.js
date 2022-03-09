cccb7403fb10eb1f272c6ef477408f9eeca82130
Arkaitz Molinuevo <arkaitz@arkaitzmolinuevo.com>
2022-03-09 20:07:07 +0100

    Plis work

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,17 @@
-//import adapter from '@sveltejs/adapter-auto';
-import cloudflare from "@sveltejs/adapter-cloudflare";
+import adapter from '@sveltejs/adapter-static';
+//import cloudflare from "@sveltejs/adapter-cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		//adapter: adapter(),
-		adapter: cloudflare(),
+		adapter: adapter(),
+		//adapter: cloudflare(),
+		csp:{
+			mode: 'auto'
+		}
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		//target: '#svelte'
 	}
 };
 
